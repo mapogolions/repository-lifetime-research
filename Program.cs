2083960e5b2910a074d0e01cfa063125ed8b883f
mapogolions <ivanovpavelalex45@gmail.com>
2022-02-23 14:20:00 +0500

    scaffold

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RepositoryLifetimeResearch;
+
+internal static class Program
+{
+    internal static void Main()
+    {
+        var services = new ServiceCollection();
+        Console.WriteLine("hello");
+    }
+}
+
+public class DbSession : DbContext
+{
+    private readonly IConfiguration _config;
+
+    public DbSession(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(_config.GetConnectionString("DefaultConnectionString"));
+    }
+}
